4612736aad21c8691a700e03d53b52e9e413b538
Javier Fernandes <javier.fernandes@gmail.com>
2017-10-10 00:03:00 -0300

    moving keyboard logic out as a trait to move with keyboard

---
+++ src/dixy/traits/MovesWithKeyboard.js
@@ -0,0 +1,68 @@
+import { keyboard } from '../../utils'
+
+export default class MovesWithKeyboard {
+
+  constructor() {
+    this.vx = 0
+    this.vy = 0
+    const left = keyboard(37)
+    const up = keyboard(38)
+    const right = keyboard(39)
+    const down = keyboard(40)
+    const self = this
+
+    Object.assign(left, {
+      press() {
+        self.vx = -5
+        self.vy = 0
+      },
+      release() {
+        if (!right.isDown && self.vy === 0) {
+          self.vx = 0
+        }
+      }
+    })
+    
+    Object.assign(up, {
+      press() {
+        self.vx = 0
+        self.vy = -5
+      },
+      release() {
+        if (!down.isDown && self.vx === 0) {
+          self.vy = 0;
+        }
+      }
+    })
+
+    Object.assign(right, {
+      press() {
+        self.vx = 5
+        self.vy = 0
+      },
+      release() {
+        if (!left.isDown && self.vy === 0) {
+          self.vx = 0
+        }
+      }
+    })
+
+    Object.assign(down, {
+      press() {
+        self.vx = 0
+        self.vy = 5
+      },
+      release() {
+        if (!up.isDown && self.vx === 0) {
+          self.vy = 0
+        }
+      }
+    })
+  }
+  
+  update(c) {
+    c.x += this.vx
+    c.y += this.vy
+  }
+
+}
--- src/dungeon/components/Explorer.js
+++ src/dungeon/components/Explorer.js
@@ -1,5 +1,6 @@
+import MovesWithKeyboard from '../../dixy/traits/MovesWithKeyboard'
 import Bounded from '../../dixy/traits/Bounded'
-import { keyboard, wall } from '../../utils'
+import { wall } from '../../utils'
 
 const { Container, Sprite } = PIXI
 
@@ -9,13 +10,11 @@ export default class Explorer extends Container {
     this.traits = []
 
     this.sprite = new Sprite(texture)
-    this.vx = 0
-    this.vy = 0
     this.addChild(this.sprite)
 
     this.on('added', ::this.componentDidMount)
     
-    this.setupKeyboard()
+    this.addTrait(new MovesWithKeyboard())
     this.addTrait(new Bounded({ 
       x1: wall.x,
       x2: wall.width,
@@ -35,66 +34,8 @@ export default class Explorer extends Container {
 
   update() {
     if (this.sprite) {
-      this.x = this.x + this.vx
-      this.y = this.y + this.vy
+      this.traits.forEach(t => t.update(this))
     }
-    this.traits.forEach(t => t.update(this))
   }
 
-  setupKeyboard() {
-    const left = keyboard(37)
-    const up = keyboard(38)
-    const right = keyboard(39)
-    const down = keyboard(40)
-    const self = this
-    
-    Object.assign(left, {
-      press() {
-        self.vx = -5
-        self.vy = 0
-      },
-      release() {
-        if (!right.isDown && self.vy === 0) {
-          self.vx = 0
-        }
-      }
-    })
-    
-    Object.assign(up, {
-      press() {
-        self.vy = -5;
-        self.vx = 0;
-      },
-      release() {
-        if (!down.isDown && self.vx === 0) {
-          self.vy = 0;
-        }
-      }
-    })
-
-    Object.assign(right, {
-      press() {
-        self.vx = 5
-        self.vy = 0
-      },
-      release() {
-        if (!left.isDown && self.vy === 0) {
-          self.vx = 0
-        }
-      }
-    })
-
-    Object.assign(down, {
-      press() {
-        self.vy = 5
-        self.vx = 0
-      },
-      release() {
-        if (!up.isDown && self.vx === 0) {
-          self.vy = 0
-        }
-      }
-    })
-
-  }
 }
